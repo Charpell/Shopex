1e26eedaeef4d31d1439b8b02ad59ac1d0df5947
charpell <charpelleumeh@gmail.com>
2019-08-04 17:50:45 +0100

    store

--- navigation/src/StoreNavigator.js
+++ navigation/src/StoreNavigator.js
@@ -1,7 +1,9 @@
 import { createStackNavigator } from 'react-navigation';
 
 import StoreHome from '../../screens/Store/home';
+import SelectedStore from '../../screens/Store/selected';
 
 export default createStackNavigator({
+  SelectedStore,
   StoreHome
 })
